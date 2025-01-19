5d01dd439ba3332c384df4cb2a685bc0fe554d6e
Justin Walraven <j.walraven@eagles.oc.edu>
2025-01-19 14:50:42 -0600

    Database for Workout, W/O Type, Muscle group, Excersise

--- app/Models/Excersise.php
+++ app/Models/Excersise.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Excersise extends Model
 {
@@ -11,4 +12,8 @@ class Excersise extends Model
     use HasFactory;
     
     protected $guarded = [];
+
+    public function muscleGroups(): BelongsTo{
+        return $this->belongsTo(MuscleGroup::class);
+    }
 }
--- app/Models/MuscleGroup.php
+++ app/Models/MuscleGroup.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MuscleGroup extends Model
 {
     /** @use HasFactory<\Database\Factories\MuscleGroupFactory> */
     use HasFactory;
+    protected $gaurded = [];
+
+    public function excersises(): HasMany{
+        return $this->hasMany(Excersise::class);
+    }
 }
