8e248ce51f832529553c883234d1cf115e379cf0
isaiahpfisher <isaiahpfisher@gmail.com>
2025-01-19 14:32:06 -0600

    workout belongs to mezocycle

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Workout extends Model
-{
-    /** @use HasFactory<\Database\Factories\WorkoutFactory> */
-    use HasFactory;
-    protected $guarded = [];
-    public function Excersise(): HasMany{
-        return $this->hasMany(Excersise::class);
-    }
+class Workout extends Model {
+  /** @use HasFactory<\Database\Factories\WorkoutFactory> */
+  use HasFactory;
+  protected $guarded = [];
 
+  public function Excersise(): HasMany {
+    return $this->hasMany(Excersise::class);
+  }
+
+  public function mezocycle(): BelongsTo {
+    return $this->belongsTo(Mezocycle::class);
+  }
 }
