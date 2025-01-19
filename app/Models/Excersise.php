185d6bda8904b80c4cf9f55648cc600b30f0f922
Justin Walraven <j.walraven@eagles.oc.edu>
2025-01-19 14:12:25 -0600

    Database models, migrations, and factories

---
+++ app/Models/Excersise.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Excersise extends Model
+{
+    /** @use HasFactory<\Database\Factories\ExcersiseFactory> */
+    use HasFactory;
+    
+    protected $guarded = [];
+}
---
+++ app/Models/WorkoutExcersise.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkoutExcersise extends Model
+{
+    /** @use HasFactory<\Database\Factories\WorkoutExcersiseFactory> */
+    use HasFactory;
+}
