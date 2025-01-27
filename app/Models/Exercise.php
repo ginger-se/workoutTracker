ad331a554ab60af9604d80d1a39a0bb9ddbb1da1
Justin Walraven <j.walraven@eagles.oc.edu>
2025-01-27 16:38:12 -0600

    exercise to workoutExercise relationship

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Exercise extends Model {
   /** @use HasFactory<\Database\Factories\ExerciseFactory> */
@@ -14,5 +15,8 @@ class Exercise extends Model {
 
   public function muscleGroups(): BelongsTo {
     return $this->belongsTo(MuscleGroup::class);
+  }
+  public function workoutExercises(): HasMany {
+    return $this->hasMany(WorkoutExercise::class);
   }
 }
